3339ba2394a2f720a31ad94d4493fb9f4c73d409
skysparko <shubhamrakhecha5@gmail.com>
2022-12-28 19:46:08 +0530

    Commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.static("content"));
 app.use(express.urlencoded({ extended: false }));
 
+//routes
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/product", productRoutes);
 
